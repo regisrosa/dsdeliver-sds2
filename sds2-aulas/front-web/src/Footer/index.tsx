8868696ade88bb356189168db483f5549b5e1d98
regisrosa <regisaraujorosa@gmail.com>
2021-01-08 18:39:13 -0300

    Add Footer

---
+++ sds2-aulas/front-web/src/Footer/index.tsx
@@ -0,0 +1,24 @@
+import './style.css';
+import {ReactComponent as YouTubeIcon} from '.youtube.svg';
+import {ReactComponent as LinkedinIcon} from '.linkedin.svg';
+import {ReactComponent as InstagramIcon} from '.instagram.svg';
+
+function Footer(){
+    return(
+        <footer className="main-footer">
+            App desenvolvido durante a segunda edição do evento Semana DevSuperior
+            <div className="footer-icons">
+                <a href="https://www.youtube.com/c/DevSuperior" target="_new">
+                    <YouTubeIcon />
+                </a>
+                <a href="https://www.linkedin.com/school/devsuperior/" target="_new">
+                    <LinkedinIcon />
+                </a>
+                <a href="https://www.instagram.com/devsuperior.ig"></a>
+                    <InstagramIcon />
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
